621166fa7072d53ffb47353a4de387467eb8161e
Isabella Becker <isabella.becker@edu.pucrs.br>
2023-11-19 00:45:20 -0300

    seleciona  2 vetores com melhor pontuação

--- Labirinto.java
+++ Labirinto.java
@@ -14,21 +14,38 @@
  * 
  * 1. Leitura do arquivo -> OK
  * 2. População inicial -> OK
- * 3. Seleção (contar colisoes e escolher os 2 que tiverem menos) se fiver algum com 0 colisoes ele achou a saida
+ * 3. Seleção (seleciona 2 com melhor pontuação) -> Parcial, faz a seleção de 2 vetores com melhor pontuação mas não descarta vetores que iniciam errado ou erram muito no inicio
  * 4. Reprodução
  * 5. Mutação (só escolhe um dos filhos e substitui alguns "genes" de forma aleatória)
+ * 6. Calcular aptidão
  */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Labirinto {
 
-    static int tamVetor = 0;
-    static int tamPopulacao = 10;
+    static int tamVetor = 10;
+    static int tamPopulacao = 5;
+
+    //Penalidade ao repetir caminho
+    static final int PEN_REPETIU = -1;
+    
+    // Penalidade ao colidir com parede
+    static final int PEN_COLIDIU = -2;
+    
+    // Penalidade ao sair do labirinto    
+    static final int PEN_SAIU = -3;
+
+    // Pontua ao acertar caminho
+    static final int PON_ACERTOU = 2;
+
+    // Pontua ao achar saida
+    static final int PON_ACHOU = 5;
 
     public static void main(String[] args) {
         char[][] labirinto = lerLabirinto("labirinto1.txt");      
@@ -37,7 +54,7 @@ public class Labirinto {
         for (int i = 0; i < labirinto.length; i++) {
             for (int j = 0; j < labirinto[i].length; j++) {
                 System.out.print(labirinto[i][j] + " ");
-                if(labirinto[i][j] == '0') tamVetor++;
+                //if(labirinto[i][j] == '0') tamVetor++;
             }
             System.out.println();
         }
@@ -66,6 +83,75 @@ public class Labirinto {
                 
             }
             System.out.println();
+        }
+    }
+
+    static class Posicao {
+        public int linha;
+        public int coluna;
+        
+        public Posicao( int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+    }
+
+    private static void calculaPontuacao(int[][] populacao, char[][] labirinto) {
+        int linha = 0;
+        int coluna = 0;       
+        
+        for (int i = 0; i < populacao.length; i++) {
+            ArrayList<Posicao> caminhoPercorrido = new ArrayList<>();
+            for (int j = 0; j < populacao[i].length-1; j++) {
+                // Caminha pelo labirinto
+                switch (populacao[i][j]) {
+                    case 0:
+                        // Anda para cima
+                        linha =+ -1;                            
+                        break;
+                    case 1:
+                        // Anda para baixo
+                        linha =+ 1;
+                        break;
+                    case 2:
+                        // Anda para esquerda
+                        coluna =+ -1;
+                        
+                        break;
+                    case 3: 
+                        // Anda para direita
+                        coluna =+ 1;
+                        break;
+                    default:
+                }
+                // Altera pontuação
+                try {                    
+                    switch (labirinto[linha][coluna]) {
+                        case '0':
+                            populacao[i][populacao[i].length-1] += PON_ACERTOU;
+                            
+                            break;
+                        case '1':
+                            populacao[i][populacao[i].length-1] += PEN_COLIDIU;
+                            break;
+                        case '3':
+                            populacao[i][populacao[i].length-1] += PON_ACHOU;
+                            break;
+                        default:                            
+                            break;                    
+                    }
+
+                    for (Posicao p : caminhoPercorrido) {
+                        if(p.linha == linha && p.coluna == coluna) {
+                            populacao[i][populacao[i].length-1] += PEN_REPETIU;
+                        }
+                    }
+                    caminhoPercorrido.add(new Posicao(linha, coluna));              
+         
+                } catch (ArrayIndexOutOfBoundsException e){
+                    populacao[i][populacao[i].length-1] += PEN_SAIU;
+                }   
+            }
         }
     }
 
@@ -107,25 +193,70 @@ public class Labirinto {
     private static int[] algoritmoGenetico(char[][] labirinto) {
         int[][] populacao = gerarPopulacao();
 
-        System.out.println("População inicial: ");
-        for (int i = 0; i < populacao.length; i++) {
-            for (int j = 0; j < populacao[i].length; j++) {
-                System.out.print(populacao[i][j] + " ");
+        System.out.println("População inicial");
+        printPopulacao(populacao);
+
+        calculaPontuacao(populacao, labirinto);
+
+        System.out.println("População inicial com Pontuação calculada: ");
+        printPopulacao(populacao);       
+
+        // selção
+        System.out.println("selecionados: ");
+        ArrayList<int[]> selecionados = selecao(populacao);
+        for (int[] s : selecionados) {
+            for(int i = 0; i < tamVetor+1; i++){
+                System.out.print(s[i] + " ");
             }
             System.out.println();
         }
 
         return null;
     }
+
+    private static ArrayList<int[]> selecao(int[][] populacao) {
+        int pontuacao1 = -1000000000;
+        int pontuacao2 = -1000000000;
+        int[] selecionado1 = new int[tamVetor + 1];
+        int[] selecionado2 = new int[tamVetor + 1];
+        ArrayList<int[]>selecionados = new ArrayList<>();
+
+        for(int i = 0; i< populacao.length; i++) {
+            if (populacao[i][populacao[i].length-1] > pontuacao1) {
+                pontuacao1 = populacao[i][populacao[i].length-1];
+                for(int j = 0; j < tamVetor+1; j++) {
+                    selecionado1[j] = populacao[i][j];
+                }
+            } 
+            else if (populacao[i][populacao[i].length-1] > pontuacao2) {
+                pontuacao2 = populacao[i][populacao[i].length-1];
+                for(int j = 0; j < tamVetor+1; j++) {
+                    selecionado2[j] = populacao[i][j];
+                }
+            }
+        }
+        selecionados.add(selecionado1);
+        selecionados.add(selecionado2);
+        return selecionados;
+    }
     
+    private static void printPopulacao(int[][] populacao) {
+        for (int i = 0; i < populacao.length; i++) {
+            for (int j = 0; j < populacao[i].length; j++) {
+                System.out.print(populacao[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     private static int[][] gerarPopulacao() {
-        int[][] populacao = new int[tamPopulacao][tamVetor];
+        //+1 para guardar a pontuação na ultima posição do vetor
+        int[][] populacao = new int[tamPopulacao][tamVetor+1];
         Random random = new Random();
 
         for (int i = 0; i < populacao.length; i++) {
-            for (int j = 0; j < populacao[i].length; j++) {
-                populacao[i][j] = random.nextInt(4);
+            for (int j = 0; j < populacao[i].length-1; j++) {
+                populacao[i][j] = random.nextInt(4);             
             }
         }        
         return populacao;
