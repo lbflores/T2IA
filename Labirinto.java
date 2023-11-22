6f977a473b45bb51e3cb6ed03dcb705d932175a8
Isabella Becker <isabellalbecker@gamil.com>
2023-11-21 21:20:19 -0300

    crossover utilizando corte com base no caminho mais correto

--- Labirinto.java
+++ Labirinto.java
@@ -29,7 +29,7 @@ import java.util.Random;
 
 public class Labirinto {
 
-    static int tamVetor = 10;
+    static int tamVetor = 0;
     static int tamPopulacao = 5;
 
     //Penalidade ao repetir caminho
@@ -54,7 +54,7 @@ public class Labirinto {
         for (int i = 0; i < labirinto.length; i++) {
             for (int j = 0; j < labirinto[i].length; j++) {
                 System.out.print(labirinto[i][j] + " ");
-                //if(labirinto[i][j] == '0') tamVetor++;
+                if(labirinto[i][j] == '0') tamVetor++;
             }
             System.out.println();
         }
@@ -98,7 +98,7 @@ public class Labirinto {
     }
 
     private static void calculaPontuacao(int[][] populacao, char[][] labirinto) {
-        Posicao posicaoAtual = new Posicao(0, 0);   
+        Posicao posicaoAtual = new Posicao(0, 0);
         
         for (int i = 0; i < populacao.length; i++) {
             ArrayList<Posicao> caminhoPercorrido = new ArrayList<>();
@@ -164,8 +164,32 @@ public class Labirinto {
         } 
     }
 
-    private static void validaCaminho() {
+    // valida o caminho guardando no último espaço do vetor o ponto em que esta certo
+    private static void validaCaminho(char[][] labirinto, int[][] populacao) {
+        for (int i = 0; i < populacao.length; i++) {
+            populacao[i][tamVetor-1] = buscaQuantMovimentoValido(i, populacao, labirinto);
+        }
+    }
 
+    private static int buscaQuantMovimentoValido(int linhaAtual,int[][] populacao, char[][] labirinto ) {
+        int quantMovimentos = 0;
+        Posicao posicaoAtual = new Posicao(0, 0);
+
+        for (int j = 0; j < populacao[linhaAtual].length-2; j++) {
+            caminhaNoLabirinto(populacao[linhaAtual][j], posicaoAtual);
+            try {
+                // se o movimento é correto add mais um
+                if(labirinto[posicaoAtual.linha][posicaoAtual.coluna] == '0' 
+                    || labirinto[posicaoAtual.linha][posicaoAtual.coluna] == '3' ) {
+                    quantMovimentos+= 1;
+                } else {
+                    return quantMovimentos;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return quantMovimentos;
+            }
+        }
+        return quantMovimentos;
     }
 
     private static char[][] caminhoNaMatriz(char[][] labirinto, int[] caminho) {
@@ -204,39 +228,119 @@ public class Labirinto {
     }
 
     private static int[] algoritmoGenetico(char[][] labirinto) {
-        int[][] populacao = gerarPopulacao();
+        int[][] populacaoAtual = gerarPopulacao();
+        int[][] populacaoIntermediaria = gerarPopulacao();
 
         System.out.println("População inicial");
-        printPopulacao(populacao);
+        printPopulacao(populacaoAtual);
 
-        calculaPontuacao(populacao, labirinto);
+        calculaPontuacao(populacaoAtual, labirinto);
+        validaCaminho(labirinto, populacaoAtual);
 
         System.out.println("População inicial com Pontuação calculada: ");
-        printPopulacao(populacao);       
+        printPopulacao(populacaoAtual);       
 
-        // selção
+        // selção por elitismo
         System.out.println("Elitismo: ");
-        int[] selecionado = elitismo(populacao);
-        for(int i = 0; i < tamVetor; i++){
+        int[] selecionado = elitismo(populacaoAtual);
+        for (int i = 0; i < tamVetor; i++) {
             System.out.print(selecionado[i] + " ");
         }
         System.out.println();
+        // coloca o selecionado por elitismo para proxima geração
+        for (int i = 0; i < tamVetor; i++) {
+            populacaoIntermediaria[0][i] = selecionado[i];
+        }
+
+        // Faz seleção por torneio para selecionar pai e mãe
+        for (int i = 1; i < populacaoIntermediaria.length; i+=2) { // a partir de 1 para manter a linha do elitismo
+            int indicePai = torneio(populacaoAtual, labirinto);
+            int indiceMae = torneio(populacaoAtual, labirinto);
+            
+            //fazer crossover usando mascara binária    
+            int[][] filhos = crossover(populacaoAtual[indicePai], populacaoAtual[indiceMae]);
+
+            populacaoIntermediaria[i] = filhos[0]; // Primeiro filho
+            populacaoIntermediaria[i + 1] = filhos[1]; // Segundo filho           
+
+            // // Avalia o filho 1
+            // int pontuacaoFilho1 = funcHeuristica(populacaoIntermediaria[i], labirinto);
+            // System.out.println("Pontuação do filho 1: " + pontuacaoFilho1);
+            // // Avalia o filho 2
+            // int pontuacaoFilho2 = funcHeuristica(populacaoIntermediaria[i + 1], labirinto);
+            // System.out.println("Pontuação do filho 2: " + pontuacaoFilho2);
+            
+            // //Aplica mutação
+            // populacaoIntermediaria[i] = mutacao(populacaoIntermediaria[i], labirinto);
+            // populacaoIntermediaria[i + 1] = mutacao(populacaoIntermediaria[i + 1], labirinto);               
+            
+        }
 
         return null;
+    }
+
+    private static int[][] crossover(int[] pai, int[] mae) {
+        Random random = new Random();
+        int corte = pai[tamVetor-1];
+        if(corte < mae[tamVetor-1]){
+            corte = mae[tamVetor-1];
+        }
+    
+        // máscara binária
+        int[] mascaraBinaria = new int[tamVetor];
+        for (int i = 0; i < tamVetor; i++) {
+            mascaraBinaria[i] = random.nextInt(2); // 0 ou 1
+        }
+    
+        // Aplica a máscara para gerar o primeiro filho
+        int[] filho1 = new int[tamVetor];
+        for (int i = 0; i < tamVetor; i++) {
+            if (i < pai[tamVetor-1]) {
+                filho1[i] = pai[i];
+            } else {
+                // Se for 1 na máscara, herda do pai, senão, herda da mãe
+                filho1[i] = (mascaraBinaria[i] == 1) ? pai[i] : mae[i];
+            }
+        }
+
+        // Aplica a máscara invertida para gerar o segundo filho
+        int[] filho2 = new int[tamVetor];
+        for (int i = 0; i < tamVetor; i++) {
+            // Se for 0 na máscara, herda do pai, senão, herda da mãe
+            filho2[i] = (mascaraBinaria[i] == 0) ? pai[i] : mae[i];
+        }
+
+        // retornar vetor com os dois filhos
+        return new int[][] { filho1, filho2 };
     }
 
     private static int[] elitismo(int[][] populacao) {
         int[] selecionado = new int[tamVetor];
         selecionado[tamVetor-2] = -1000000;
 
-        for(int i = 0; i< populacao.length; i++) {          
+        for (int i = 0; i< populacao.length; i++) {          
             if (populacao[i][populacao[i].length-2] >= selecionado[tamVetor-2]) {
-                for(int j = 0; j < tamVetor; j++) {
+                for (int j = 0; j < tamVetor; j++) {
                     selecionado[j] = populacao[i][j];
                 }
             }
         }
         return selecionado;
+    }
+
+    private static int torneio(int[][] populacaoAtual, char[][] labirinto) {
+        Random random = new Random();
+    
+        // Seleciona dois indivíduos aleatórios
+        int indice1 = random.nextInt(populacaoAtual.length);
+        int indice2 = random.nextInt(populacaoAtual.length);
+    
+        // Avalia os dois indivíduos no torneio
+        int pontuacao1 = populacaoAtual[indice1][tamVetor-2];
+        int pontuacao2 = populacaoAtual[indice2][tamVetor-2];
+    
+        // Retorna o índice do indivíduo com maior pontuação
+        return (pontuacao1 > pontuacao2) ? indice1 : indice2;
     }
     
     private static void printPopulacao(int[][] populacao) {
